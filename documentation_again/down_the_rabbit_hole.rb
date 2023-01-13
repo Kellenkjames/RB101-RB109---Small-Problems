c899a530f543fa2a9c3e02d7f748fb4e04a0a1a6
Kellen James <Kellenkjames@gmail.com>
2023-01-13 16:16:03 -0500

    completes finding YAML documentation

---
+++ documentation_again/down_the_rabbit_hole.rb
@@ -0,0 +1,8 @@
+# Down the Rabbit Hole
+
+# Find the documentation for YAML::load_file
+
+require 'yaml'
+MESSAGES = YAML.load_file('calculator_messages.yml')
+
+# https://docs.ruby-lang.org/en/3.2/Psych.html
