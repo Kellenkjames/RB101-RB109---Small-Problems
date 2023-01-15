7b444ef045d6b94fd1bb7b02c6557bd40a07c5ee
Kellen James <Kellenkjames@gmail.com>
2023-01-15 16:27:12 -0500

    add notes to understand the problem description

---
+++ easy_1/repeat_yourself.rb
@@ -0,0 +1,20 @@
+# Repeat Yourself
+
+=begin
+
+* PROBLEM: Write a method that takes two arguments, a string and a positive integer, and prints the string as many times as the integer indicates.
+
+- input(s): string, integer
+- output: string 
+- rules:
+        Explicit requirements: 
+        - integer must be positive 
+        - string must be printed as many times as the integer indicates
+
+        Implicit requirements:
+        - integer cannot be negative or a floating point
+        - string will be printed (not returned)
+
+
+
+=end 
