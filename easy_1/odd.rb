87f831f03d7809940939a8befd65c2d0754bb5e6
Kellen James <Kellenkjames@gmail.com>
2023-01-16 11:10:50 -0500

    Test remaining cases

--- easy_1/odd.rb
+++ easy_1/odd.rb
@@ -59,8 +59,9 @@ def is_odd?(number)
   number.abs.odd? ? true : false
 end
 
-puts is_odd?(2)
-
-
-
-
+puts is_odd?(2)    # => false
+puts is_odd?(5)    # => true
+puts is_odd?(-17)  # => true
+puts is_odd?(-8)   # => false
+puts is_odd?(0)    # => false
+puts is_odd?(7)    # => true
