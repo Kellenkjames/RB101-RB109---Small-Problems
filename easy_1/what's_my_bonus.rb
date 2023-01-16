2cf9d441ba66d344e9023d75203ed0b62ed1ce8b
Kellen James <Kellenkjames@gmail.com>
2023-01-16 14:20:33 -0500

    Add pseudocode to better understand the problem description

---
+++ easy_1/what's_my_bonus.rb
@@ -0,0 +1,28 @@
+# What's my Bonus?
+
+=begin
+
+* P - [Understand the] Problem
+
+PROBLEM: Write a method that takes two arguments, a positive integer and a boolean, and calculates the bonus for a given salary. If the boolean is true, the bonus should be half of the salary. If the boolean is false, the bonus should be 0.
+
+- input(s): integer, boolean
+- output: boolean
+- rules:
+          - Explicit Requirements:
+            - Accepts two arguments: a positive integer and a boolean
+            - Calculates the bonus for a given salary
+            - If the boolean is true, the bonus should be half of the salary
+              - Otherwise, the bonus should be 0
+            
+          - Implicit Requirements:
+            - Integer cannot be zero or negative
+
+
+
+
+
+
+
+
+=end
