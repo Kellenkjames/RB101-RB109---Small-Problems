3bb637dda0105f4a53991a408ae319e13e039b68
Kellen James <Kellenkjames@gmail.com>
2023-01-16 10:44:21 -0500

    Add test cases to new section of pseudocode

--- easy_1/odd.rb
+++ easy_1/odd.rb
@@ -21,4 +21,14 @@ PROBLEM: Write a method that takes one integer argument, which may be positive, 
             
 __________________________________________________
 
+* E - Examples / Test Cases
+
+puts is_odd?(2)    # => false
+puts is_odd?(5)    # => true
+puts is_odd?(-17)  # => true
+puts is_odd?(-8)   # => false
+puts is_odd?(0)    # => false
+puts is_odd?(7)    # => true
+
+
 =end 
