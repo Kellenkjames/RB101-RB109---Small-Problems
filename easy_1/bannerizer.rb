79b2335a04cbe667cec094da7198ad4090073aed
Kellen James <Kellenkjames@gmail.com>
2023-01-16 14:59:58 -0500

    Add pseudocode to understand the problem

---
+++ easy_1/bannerizer.rb
@@ -0,0 +1,24 @@
+# Bannerizer
+
+=begin
+
+* P - [Understand the] Problem
+
+PROBLEM: Write a method that will take a short line of text, and print it within a box.
+
+input: string
+output: string (within a box)
+rules:
+        - Explicit Requirements:
+          - Method will take a short line of text
+          - Print it within a box
+
+        - Implicit Requirements:
+          - The input should always fit in your terminal window
+
+        - Clarifying Questions:
+          - Do we need to worry about any other objects that are not strings? 
+
+__________________________________________________
+
+=end
