bb04a65fde7231c6e815bec2b229054d6b7405dd
Kellen James <Kellenkjames@gmail.com>
2023-01-16 13:18:33 -0500

    Add test cases below solution - all tests pass

--- easy_1/stringy_strings.rb
+++ easy_1/stringy_strings.rb
@@ -51,11 +51,11 @@ end
 
 __________________________________________________
 
-* C - Code
-
 =end
 
 #_________________________________________________
+
+#* C - Code
 
 def stringy(number)
 str = ''
@@ -70,4 +70,7 @@ end
 str
 end
 
-p stringy(6)
+puts stringy(6) == '101010'
+puts stringy(9) == '101010101'
+puts stringy(4) == '1010'
+puts stringy(7) == '1010101'
