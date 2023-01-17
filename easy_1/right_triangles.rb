631eb23708518569070c17105acd4c07c0b4db81
Kellen James <Kellenkjames@gmail.com>
2023-01-17 10:41:41 -0500

    Add high level problem description with expected inputs, outputs, and rules

---
+++ easy_1/right_triangles.rb
@@ -0,0 +1,28 @@
+# Right Triangles
+
+=begin
+
+* P - [Understand the] Problem
+
+PROBLEM:
+
+Write a method that takes a positive integer, n, as an argument, and displays a right triangle whose sides each have n stars. The hypotenuse of the triangle (the diagonal side in the images below)should have one end at the lower-left of the triangle, and the other end at the upper-right.
+
+- input: integer
+- output: string
+- rules:
+        Explicit Requirements:
+        - method takes a positive integer, n, as an argument
+        - displays a right triangle whose "sides" each have "n" stars
+        - hypotenuse of the triangle (diagonal side) should have:
+          - one end at the lower-left of the triangle
+          - other end at the upper-right
+
+        Implicit Requirements:
+        - integer must be positive
+        - integer represents:
+          - total row count
+          - count of stars in the first row
+      
+
+=end
