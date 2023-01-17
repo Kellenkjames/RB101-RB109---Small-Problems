d9d12ca2c6cbdc2ecfbfe1683b01eb9064c6c31f
Kellen James <Kellenkjames@gmail.com>
2023-01-17 17:03:31 -0500

    Checkpoint, first test is passing with un-optimized solution

--- easy_1/right_triangles.rb
+++ easy_1/right_triangles.rb
@@ -6,14 +6,14 @@
 
 PROBLEM:
 
-Write a method that takes a positive integer, n, as an argument, and displays a right triangle whose sides each have n stars. The hypotenuse of the triangle (the diagonal side in the images below)should have one end at the lower-left of the triangle, and the other end at the upper-right.
+Write a method that takes a positive integer, n, as an argument, and displays a right triangle whose rows each have n stars. The hypotenuse of the triangle (the diagonal side in the images below)should have one end at the lower-left of the triangle, and the other end at the upper-right.
 
 - input: integer
 - output: string
 - rules:
         Explicit Requirements:
         - method takes a positive integer, n, as an argument
-        - displays a right triangle whose "sides" each have "n" stars
+        - displays a right triangle whose "rows" each have "n" stars
         - hypotenuse of the triangle (diagonal side) should have:
           - one end at the lower-left of the triangle
           - other end at the upper-right
@@ -22,7 +22,6 @@ Write a method that takes a positive integer, n, as an argument, and displays a 
         - integer must be positive
         - integer represents:
           - total row count
-          - count of stars in the first row
 
 __________________________________________________
 
@@ -52,23 +51,32 @@ __________________________________________________
 
 * D - Data Structure
 
-Array(5) 
+Array(5)
 
-["", "", "", "", *]          4 spaces,    1 star
-["", "", "", *, *]           3 spaces,    2 stars
-["", "", *, *, *]            2 spaces,    3 stars
-["", *, *, *, *]             1 space,      4 stars
-[*, *, *, *, *]              0 spaces,    5 stars
+[
+  ["", "", "", "", *]          4 spaces,    1 star
+  ["", "", "", *, *]           3 spaces,    2 stars
+  ["", "", *, *, *]            2 spaces,    3 stars
+  ["", *, *, *, *]             1 space,      4 stars
+  [*, *, *, *, *]              0 spaces,    5 stars
+]
 
 __________________________________________________
 
 * A - Algorithm
 
-- Initialize an empty array and assign to variable named arr
-- Initialize a counter variable and set equal to 0
-- Initialize a 
-- While counter < number:
-    
+- Initialize an empty array and assign to variable called arr
+- Iterate as many times as the number argument:
+  - print stars for each row:
+    - rows = '*' * number
+    - arr << rows
+  - Iterate over the arr:
+    - Replace stars with empty spaces (descending order)
+    - 
+
+end
+
+arr
 end
 
 __________________________________________________
@@ -80,7 +88,27 @@ __________________________________________________
 #* C - Code
 
 def triangle(number)
+  arr = []
   
+  counter = 0
+  loop do
+    counter += 1
+    rows = '*' * counter
+    arr << rows
+    break if counter == number
+  end
+
+  counter_two = 0
+  empty_spaces = number
+  loop do
+    empty_spaces -= 1
+    empty_spaces_multiplier = ' ' * empty_spaces
+    arr[counter_two].prepend(empty_spaces_multiplier)
+    counter_two += 1
+    break if counter_two == number
+  end
+
+puts arr
 end
 
 triangle(5)
