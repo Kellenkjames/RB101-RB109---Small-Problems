88ee680fc7add73c5104c39ab397007dd59eb31e
Kellen James <Kellenkjames@gmail.com>
2023-01-17 18:51:52 -0500

    Add additional madlib examples and random sampling to improve user experience

--- easy_1/madlibs.rb
+++ easy_1/madlibs.rb
@@ -65,17 +65,20 @@ __________________________________________________
 
 #* C - Code
 
-puts "Enter a noun:"
+puts 'Enter a noun: '
 noun = gets.chomp
 
-puts "Enter a verb:"
+puts 'Enter a verb: '
 verb = gets.chomp
 
-puts "Enter an adjective:"
+puts 'Enter an adjective: '
 adjective = gets.chomp
 
-puts "Enter an adverb:"
+puts 'Enter an adverb: '
 adverb = gets.chomp
 
-madlib = "Do you #{verb} your #{adjective} #{noun} quickly? That's hilarious!"
-puts madlib
+sentence_1 = "Do you #{verb} your #{adjective} #{noun} #{adverb}? That's hilarious!"
+sentence_2 = "The #{adjective} #{noun} #{verb}s #{adverb} over the lazy dog."
+sentence_3 = "The #{noun} #{adverb} #{verb}s up #{adjective} Khali's turle."
+
+puts [sentence_1, sentence_2, sentence_3].sample
