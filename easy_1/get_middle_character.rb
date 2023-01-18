1a4fbc1cbabfdeb91841e65f58e8344edd01a1fd
Kellen James <Kellenkjames@gmail.com>
2023-01-18 11:48:43 -0500

    Add pseudocode to understand the problem description

---
+++ easy_1/get_middle_character.rb
@@ -0,0 +1,23 @@
+# Get Middle Character
+
+=begin
+
+* P - [Understanding the] Problem
+
+PROBLEM: Write a method that takes a non-empty string argument, and returns the middle character or characters of the argument. If the argument has an odd length, you should return exactly one character. If the argument has an even length, you should return exactly two characters.
+
+input: string
+output: string
+rules:
+        - Explicit Requirements:
+          - method takes a non-empty string argument
+          - returns the middle characters of the argument
+          - if the agrument has an odd length
+            - return exactly one character
+          - otherwise, argument has an even length
+            - return exactly two characters
+
+        - Implicit Requirements:
+            - argument must be a non-empty string
+
+=end
