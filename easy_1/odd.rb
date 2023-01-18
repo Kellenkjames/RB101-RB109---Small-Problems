f8ff55c0249ad5a75b11d916ca9f53d5a4257ed1
Kellen James <Kellenkjames@gmail.com>
2023-01-18 13:13:39 -0500

    Update title casing in pseudocode for consistency

--- easy_1/odd.rb
+++ easy_1/odd.rb
@@ -4,20 +4,20 @@
 
 * P - [Understanding the] Problem
 
-PROBLEM: Write a method that takes one integer argument, which may be positive, negative, or zero. This method returns true if the number's absolute value is odd. You may assume that the argument is a valid integer value.
+Problem: Write a method that takes one integer argument, which may be positive, negative, or zero. This method returns true if the number's absolute value is odd. You may assume that the argument is a valid integer value.
 
 - input: integer
 - output: boolean
 - rules:
           - Explicit Requirements:
-            - integer argument can be positive, negative, or zero
-            - method returns true if the number's absolute value is odd
+            - Integer argument can be positive, negative, or zero
+            - Method returns true if the number's absolute value is odd
 
           - Implicit Requirements:
             - The argument is a valid integer value
 
           Clarifying Questions:
-            - What do we mean by absolute value? 
+            - What do we mean by absolute value?
             
 __________________________________________________
 
