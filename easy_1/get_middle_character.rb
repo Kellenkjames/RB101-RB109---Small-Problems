5dbc98d875db930ce52ed8decf72f5de4d3d7129
Kellen James <Kellenkjames@gmail.com>
2023-01-18 11:52:30 -0500

    Update, grammatical fixes in pseudocode

--- easy_1/get_middle_character.rb
+++ easy_1/get_middle_character.rb
@@ -4,7 +4,7 @@
 
 * P - [Understanding the] Problem
 
-PROBLEM: Write a method that takes a non-empty string argument, and returns the middle character or characters of the argument. If the argument has an odd length, you should return exactly one character. If the argument has an even length, you should return exactly two characters.
+Problem: Write a method that takes a non-empty string argument, and returns the middle character or characters of the argument. If the argument has an odd length, you should return exactly one character. If the argument has an even length, you should return exactly two characters.
 
 input: string
 output: string
@@ -12,7 +12,7 @@ rules:
         - Explicit Requirements:
           - method takes a non-empty string argument
           - returns the middle characters of the argument
-          - if the agrument has an odd length
+          - if the argument has an odd length
             - return exactly one character
           - otherwise, argument has an even length
             - return exactly two characters
