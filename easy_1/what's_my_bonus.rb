ebdff6e62bec099900161ca1671a37b7146589e7
Kellen James <Kellenkjames@gmail.com>
2023-01-18 13:27:20 -0500

    Update title casing for pseudocode for consistency

--- easy_1/what's_my_bonus.rb
+++ easy_1/what's_my_bonus.rb
@@ -4,7 +4,7 @@
 
 * P - [Understand the] Problem
 
-PROBLEM: Write a method that takes two arguments, a positive integer and a boolean, and calculates the bonus for a given salary. If the boolean is true, the bonus should be half of the salary. If the boolean is false, the bonus should be 0.
+Problem: Write a method that takes two arguments, a positive integer and a boolean, and calculates the bonus for a given salary. If the boolean is true, the bonus should be half of the salary. If the boolean is false, the bonus should be 0.
 
 - input(s): integer, boolean
 - output: boolean
@@ -56,6 +56,6 @@ def calculate_bonus(salary, bonus)
   bonus ? (salary / 2) : 0
 end
 
-puts calculate_bonus(2800, true) == 1400
-puts calculate_bonus(1000, false) == 0
+calculate_bonus(2800, true) == 1400
+calculate_bonus(1000, false) == 0
 puts calculate_bonus(50000, true) == 25000
