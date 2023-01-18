823ce86c5d22812a8568bd0527d4e3f67cf904eb
Kellen James <Kellenkjames@gmail.com>
2023-01-18 10:46:06 -0500

    Add pseudocode to understand the problem in new file

---
+++ easy_1/reverse_digits_in_number.rb
@@ -0,0 +1,24 @@
+# Reverse the Digits in a Number
+
+=begin
+
+* P - [Understanding the Problem]
+
+PROBLEM: Write a method that takes a positive integer as an argument and returns that number with its digits reversed.
+
+input: integer
+output: boolean
+rules:
+        - Explicit Requirements:
+          - integer must be positive
+          - return integer with its digits reversed
+
+        - Implicit Requirements:
+          - ignore arguments with leading zeros
+
+        - Clarifying Questions:
+          - Are we returning the same object or a new object?
+
+__________________________________________________
+
+=end
