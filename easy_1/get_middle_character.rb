fd68e0a71ee8cf4ae097cf19ac52f6798f2e21a1
Kellen James <Kellenkjames@gmail.com>
2023-01-18 11:50:58 -0500

    Add examples / test cases

--- easy_1/get_middle_character.rb
+++ easy_1/get_middle_character.rb
@@ -20,4 +20,16 @@ rules:
         - Implicit Requirements:
             - argument must be a non-empty string
 
+__________________________________________________
+
+* E - Examples / Test Cases
+
+center_of('I love ruby') == 'e'
+center_of('Launch School') == ' '
+center_of('Launch') == 'un'
+center_of('Launchschool') == 'hs'
+center_of('x') == 'x'
+
+__________________________________________________
+
 =end
