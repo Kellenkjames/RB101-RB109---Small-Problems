f3f388d500814a85ccc844116b867a869d639f16
Kellen James <Kellenkjames@gmail.com>
2023-01-18 13:25:39 -0500

    Update title casing in pseudocode for consistency

--- easy_1/stringy_strings.rb
+++ easy_1/stringy_strings.rb
@@ -4,7 +4,7 @@
 
 * P - [Understanding the] Problem
 
-PROBLEM: Write a method that takes one argument, a positive integer, and returns a string of alternating 1s and 0s, always starting with 1. The length of the string should match the given integer.
+Problem: Write a method that takes one argument, a positive integer, and returns a string of alternating 1s and 0s, always starting with 1. The length of the string should match the given integer.
 
 - input: integer
 - output: string
@@ -69,7 +69,7 @@ end
 arr.join
 end
 
-puts stringy(6) == '101010'
-puts stringy(9) == '101010101'
-puts stringy(4) == '1010'
-puts stringy(7) == '1010101'
+stringy(6) == '101010'
+stringy(9) == '101010101'
+stringy(4) == '1010'
+stringy(7) == '1010101'
