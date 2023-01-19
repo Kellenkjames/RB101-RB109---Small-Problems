28fc99c1792646f2a67aa49a7ecd4c7be8c08d49
Kellen James <Kellenkjames@gmail.com>
2023-01-19 13:21:49 -0500

    Update variable names to improve readability of program

--- easy_2/tip_calculator.rb
+++ easy_2/tip_calculator.rb
@@ -41,13 +41,13 @@ __________________________________________________
 - bill_amount = gets.chomp.to_f
 
 - Prompt user for the tip percentage
-- tip_rate = gets.chomp.to_f
+- percentage = gets.chomp.to_f
 
-- tip_amount = ((bill_amount * tip_rate) / 100).round(2)
-- total = (bill_amount + tip_amount).round(2)
+- tip = ((bill_amount * percentage) / 100).round(2)
+- total = (bill_amount + tip).round(2)
 
-puts "The tip is #{tip_amount}"
-puts "The total is #{total}"
+puts "The tip is $#{tip}"
+puts "The total is $#{total}"
 
 __________________________________________________
 
@@ -62,10 +62,10 @@ puts 'What is the bill?'
 bill_amount = gets.chomp.to_f
 
 puts 'What is the tip percentage?'
-tip_rate = gets.chomp.to_f
+percentage = gets.chomp.to_f
 
-tip_amount = ((bill_amount * tip_rate) / 100).round(2)
-total = (bill_amount + tip_amount).round(2)
+tip = ((bill_amount * percentage) / 100).round(2)
+total = (bill_amount + tip).round(2)
 
-puts "==> The tip is $#{tip_amount}"
+puts "==> The tip is $#{tip}"
 puts "==> The total is $#{total}"
