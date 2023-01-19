d144468c5bc935e81ec8fb2df2dbb3471d531335
Kellen James <Kellenkjames@gmail.com>
2023-01-19 14:22:09 -0500

    Add examples and test cases for problem

--- easy_2/when_will_i_retire.rb
+++ easy_2/when_will_i_retire.rb
@@ -18,4 +18,15 @@ rules:
 
 __________________________________________________
 
+* E - Examples / Test Cases
+
+What is your age? 30
+At what age would you like to retire? 70
+
+It's 2016. You will retire in 2056.
+You have only 40 years of work to go!
+
+__________________________________________________
+
+
 =end
