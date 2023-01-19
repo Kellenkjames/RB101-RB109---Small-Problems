e911db50e8b8c7cbc02211fbca7813df456843bb
Kellen James <Kellenkjames@gmail.com>
2023-01-19 16:49:23 -0500

    Add pseudocode to breakdown problem description

---
+++ easy_2/odd_numbers.rb
@@ -0,0 +1,24 @@
+# Odd Numbers
+
+=begin
+
+* P - [Understanding the] Problem
+
+Problem: Print all odd numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
+
+input: integers
+output: integers
+rules:
+        - Explicit Requirements:
+          - Print all odd numbers from 1 to 99 (inclusive)
+          - Each number needs to be on it's own separate line
+
+        - Implicit Requirements
+          - Include the numbers on the outer bounds of the range
+
+__________________________________________________
+
+
+
+
+=end
