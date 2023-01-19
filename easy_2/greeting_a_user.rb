189e09c67424600008e12a76b3b8d315cf97167f
Kellen James <Kellenkjames@gmail.com>
2023-01-19 15:49:54 -0500

    Add pseudocode to understand the problem description

---
+++ easy_2/greeting_a_user.rb
@@ -0,0 +1,21 @@
+# Greeting a user
+
+=begin
+
+* P - [Understanding the] Problem
+
+Problem: Write a program that will ask for user's name. The program will then greet the user. If the user writes "name!" then the computer yells back to the user.
+
+input: string
+output: string
+rules:
+        - Explicit Requirements:
+          - Ask for user's name
+          - Make program greet the user
+          - If the user writes "name!" then the computer yells back to the user
+
+        - Implicit Requirements:
+          - Non-string objects are not allowed
+        
+
+=end
