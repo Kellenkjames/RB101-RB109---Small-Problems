4b18974b2245a270f020ba54ec46667cd23da620
Kellen James <Kellenkjames@gmail.com>
2023-01-19 17:18:40 -0500

    Add new file and problem breakdown in pseudocode

---
+++ easy_2/even_numbers.rb
@@ -0,0 +1,22 @@
+# Even Numbers 
+
+=begin
+
+* P - [Understanding the] Problem
+
+Problem: Print all even numbers from 1 to 99, inclusive, to the console, with each number on a separate line.
+
+input: integers
+output: integers
+rules:
+        - Explicit Requirements:
+          - Print all even numbers from 1 to 99 (inclusive)
+          - Each number should be printed on a separate line
+
+        - Implicit Requirements:
+          - Include the numbers on the outer bounds of the range, i.e., 1 and 99 (if applicable)
+
+
+
+
+=end
