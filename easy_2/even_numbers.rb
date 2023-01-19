d764462b37f16ed89e39134b00250010acc33a48
Kellen James <Kellenkjames@gmail.com>
2023-01-19 17:20:30 -0500

    Add examples and test cases

--- easy_2/even_numbers.rb
+++ easy_2/even_numbers.rb
@@ -16,7 +16,17 @@ rules:
         - Implicit Requirements:
           - Include the numbers on the outer bounds of the range, i.e., 1 and 99 (if applicable)
 
+__________________________________________________
 
+* E - Examples / Test Cases
 
+2
+4
+6
+8
+10
+...
+
+__________________________________________________
 
 =end
