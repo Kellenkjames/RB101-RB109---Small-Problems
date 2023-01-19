976784f1c2c06666a8b2692d5b0cffc23ad5e8ef
Kellen James <Kellenkjames@gmail.com>
2023-01-19 10:45:02 -0500

    Add new file and pseudocode to understand the problem

---
+++ how_old_is_teddy.rb
@@ -0,0 +1,20 @@
+# How old is Teddy?
+
+=begin
+
+* P - [Understanding the] Problem
+
+Problem: Build a program that randomly generates and prints Teddy's age. To get the age, you should generate a random number between 20 and 200.
+
+input: n/a
+output: string
+rules:
+        - Explicit Requirements:
+          - Generate a random number between 20 and 200
+
+        - Implicit Requirements:
+          - method doesn't need to return a value (only needs to print)
+
+__________________________________________________
+
+=end 
