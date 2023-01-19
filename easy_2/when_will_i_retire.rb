f310d572115cc32a3688a0fec380e08a577c10d9
Kellen James <Kellenkjames@gmail.com>
2023-01-19 14:21:03 -0500

    Add pseudocode to understand the problem description

---
+++ easy_2/when_will_i_retire.rb
@@ -0,0 +1,21 @@
+# When will I Retire?
+
+=begin
+
+* P - [Understanding the] Problem
+
+Problem: Build a program that displays when the user will retire and how many years she has to work till retirement.
+
+input: string, string
+output: string
+rules:
+        - Explicit Requirements
+          -  Display when the user will retire
+          - Display how many years he / she has to work till retirement
+
+        - Implicit Requirements
+          - User inputer (strings) will need to be converted to integers
+
+__________________________________________________
+
+=end
