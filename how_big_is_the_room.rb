ff1bd77c8bfa9a3f7444315c64e26ef347db5305
Kellen James <Kellenkjames@gmail.com>
2023-01-19 11:19:22 -0500

    Add problem description to pseudocode

---
+++ how_big_is_the_room.rb
@@ -0,0 +1,19 @@
+# How big is the room?
+
+=begin
+
+* P - [Understanding the] Problem
+
+Problem: Build a program that asks a user for the length and width of a room in meters and then displays the area of the room in both square meters and square feet.
+
+input: string, string
+output: string
+rules:
+        - Explicit Requirements:
+          - Ask the user for the length of a room in meters
+          - Ask the user for the length of a room in square feet
+
+        - Implicit Requirements:
+          - User input (string) needs to be converted to an integer
+
+=end
