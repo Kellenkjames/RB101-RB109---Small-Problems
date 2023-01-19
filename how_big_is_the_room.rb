b1f6d9efc7602a639c9ed5aca591f2b752086ce2
Kellen James <Kellenkjames@gmail.com>
2023-01-19 11:23:31 -0500

    Add examples and test cases

--- how_big_is_the_room.rb
+++ how_big_is_the_room.rb
@@ -17,4 +17,16 @@ rules:
           - User input (string) needs to be converted to an integer
           - 1 square meter == 10.7639 square feet
 
+__________________________________________________
+
+* E - Examples / Test Cases
+
+Enter the length of the room in meters:
+10
+Enter the width of the room in meters:
+7
+The area of the room is 70.0 square meters (753.47 square feet).
+
+__________________________________________________
+
 =end
