2661fa4cb2bf5b07a031d200c97adcbbf7923dcd
Kellen James <Kellenkjames@gmail.com>
2023-01-19 12:23:07 -0500

    Update, move files to easy\2 directory

---
+++ easy_2/tip_calculator.rb
@@ -0,0 +1,25 @@
+# Tip calculator
+
+=begin
+
+* P - [Understanding the] Problem
+
+Problem: Create a simple tip calculator. The program should prompt for a bill amount and a tip rate. The program must compute the tip and then display both the tip and the total amount of the bill.
+
+input: string, string
+output: string, string
+rules:
+        - Explicit Requirements:
+          - Prompt for a bill amount and a tip rate
+          - Must compute the tip
+          - Must display both the tip and the total amount of the bill
+
+        - Implicit Requirements:
+          - User input (strings) need to be converted to integers
+
+        
+
+
+
+
+=end 
