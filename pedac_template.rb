a85668f7567ee1f22a8b4f3945a42b753b50f8b5
Kellen James <Kellenkjames@gmail.com>
2023-01-20 17:31:08 -0500

    Add pedac_template starter code for future use

---
+++ pedac_template.rb
@@ -0,0 +1,40 @@
+
+
+=begin
+
+* P - [Understanding the] Problem
+
+Problem: 
+
+input: 
+output: 
+rules:
+        - Explicit Requirements:
+          -
+
+        - Implicit Requirements:
+          - 
+
+        - Clarifying Questions:
+          -
+
+__________________________________________________
+
+* E - Examples / Test Cases
+
+__________________________________________________
+
+* D - Data Structure
+
+__________________________________________________
+
+* A - Algorithm
+
+__________________________________________________
+
+=end
+
+#_________________________________________________
+
+#* C - Code
+
