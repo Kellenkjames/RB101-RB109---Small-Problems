382fd70706df410770d075ca9284c353770bf064
Kellen James <Kellenkjames@gmail.com>
2023-01-20 13:45:55 -0500

    Checkpoint, add base code for algorithm

--- easy_2/sum_or_product.rb
+++ easy_2/sum_or_product.rb
@@ -40,10 +40,18 @@ __________________________________________________
 * A - Algorithm
 
 - Initialize an empty array and assign to a variable named arr
-- Prompt the user to enter an integer greater than 0:
-- integer = gets.chomp.to_i
+- integer = nil 
+
+- Prompt the user to enter an integer greater than 0
+- Keep asking the user for input until integer is greater than 5
+  - If integer is greater than 5
+    - break from loop (continue)
+  - Else
+    - ask user again
+  -end
 
 - Prompt the user to either enter 's' to compute the sum, or 'p' to compute the product
+- Repeat until user either enter 's' or 'p' 
 - If user enters 's'
   - compute the sum
 - Elsif user enters 'p'
@@ -59,3 +67,35 @@ __________________________________________________
 =end
 
 #_________________________________________________
+
+#* C - Code
+
+arr = []
+
+loop do
+  puts '>> Please enter an integer greater than 0:'
+  integer = gets.chomp.to_i
+  
+  break if integer > 5
+  
+  loop do
+    puts ">> Please 's' to compute the sum, 'p' to compute the product."
+    answer = gets.chomp.downcase
+    
+    if answer == 's'
+      # compute_the sum
+    elsif answer == 'p'
+      # compute_the product
+    else
+      puts ">> Please enter 's' to compute the sum or 'p' to compute the product."
+    end
+    
+    break if answer %w(s, p)
+  end
+
+  answer
+end
+
+
+
+
