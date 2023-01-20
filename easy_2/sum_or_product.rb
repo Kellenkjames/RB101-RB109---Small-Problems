43931be3d08f0cc2611ac5a164233b4e6128105c
Kellen James <Kellenkjames@gmail.com>
2023-01-20 13:03:03 -0500

    Add new file and high level description of problem

---
+++ easy_2/sum_or_product.rb
@@ -0,0 +1,21 @@
+# Sum or Product of Consecutive Integers
+
+=begin
+
+Problem: Write a program that asks the user to enter an integer greater than 0, then asks if the user wants to determine the sum or product of all numbers between 1 and the entered integer.
+
+input: string
+output: string
+rules:
+        - Explicit Requirements:
+          - Ask the user to enter an integer greater than 0
+          - Ask if the user wants to determine the sum or product of all numbers between 1 and the entered integer
+
+        - Implicit Requirements:
+          - Input (string) needs to be converted to integer
+
+
+__________________________________________________
+
+
+=end 
