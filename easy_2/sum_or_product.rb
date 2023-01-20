3db45226c7995105db7fa874e8a595f0466786ec
Kellen James <Kellenkjames@gmail.com>
2023-01-20 13:07:45 -0500

    Add examples and test cases to pseudocode

--- easy_2/sum_or_product.rb
+++ easy_2/sum_or_product.rb
@@ -2,6 +2,8 @@
 
 =begin
 
+* P - [Understanding the] Problem
+
 Problem: Write a program that asks the user to enter an integer greater than 0, then asks if the user wants to determine the sum or product of all numbers between 1 and the entered integer.
 
 input: string
@@ -14,6 +16,18 @@ rules:
         - Implicit Requirements:
           - Input (string) needs to be converted to integer
 
+        - Clarifying Questions:
+          - Do we need to check for input validation?
+
+__________________________________________________
+
+* E - Examples / Test Cases
+
+>> Please enter an integer greater than 0:
+5
+>> Enter 's' to compute the sum, 'p' to compute the product. 
+s
+The sum of the integers between 1 and 5 is 15.
 
 __________________________________________________
 
