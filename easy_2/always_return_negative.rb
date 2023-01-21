a20f9051e4be1d0355b877fd1b81aac0495480b0
Kellen James <Kellenkjames@gmail.com>
2023-01-21 17:41:15 -0500

    Add new file with pseudocode to understand the problem

---
+++ easy_2/always_return_negative.rb
@@ -0,0 +1,42 @@
+# Always Return Negative
+
+=begin
+
+* P - [Understanding the] Problem
+
+Problem: Write a method that takes a number as an argument. If the argument is a positive number, return the negative of that number. If the number is 0 or negative, return the original number.
+
+input: integer
+output: boolean
+rules:
+        - Explicit Requirements:
+          - Method takes a number as an argument
+          - If the argument is a positive number, return the negative of that number
+          - If the number is 0 or negative, return the original number
+
+        - Implicit Requirements:
+          - No floating point numbers
+
+        - Clarifying Questions:
+          - N / A
+
+__________________________________________________
+
+* E - Examples / Test Cases
+
+__________________________________________________
+
+* D - Data Structure
+
+__________________________________________________
+
+* A - Algorithm
+
+__________________________________________________
+
+=end
+
+#_________________________________________________
+
+#* C - Code
+
