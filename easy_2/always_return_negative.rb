eeea6d9272fde175973bb303da27c3442e126e32
Kellen James <Kellenkjames@gmail.com>
2023-01-21 17:51:54 -0500

    Add code solution with test cases

--- easy_2/always_return_negative.rb
+++ easy_2/always_return_negative.rb
@@ -52,3 +52,14 @@ __________________________________________________
 
 #* C - Code
 
+def negative(number)
+  if number.positive?
+    -number
+  elsif number == 0 || number.negative? 
+    number
+  end
+end
+
+puts negative(5) == -5
+puts negative(-3) == -3
+puts negative(0) == 0      # There's no such thing as -0 in ruby
