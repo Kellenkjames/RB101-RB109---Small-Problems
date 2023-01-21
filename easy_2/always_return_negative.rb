f0e89881806379f4e8e0f258ce035a00ea26a27f
Kellen James <Kellenkjames@gmail.com>
2023-01-21 17:55:55 -0500

    Update, remove puts statements for test cases

--- easy_2/always_return_negative.rb
+++ easy_2/always_return_negative.rb
@@ -60,6 +60,6 @@ def negative(number)
   end
 end
 
-puts negative(5) == -5
-puts negative(-3) == -3
-puts negative(0) == 0      # There's no such thing as -0 in ruby
+negative(5) == -5
+negative(-3) == -3
+negative(0) == 0      # There's no such thing as -0 in ruby
