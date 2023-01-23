5dde0bbbf6a15483ed94796fe162a9e14253bfbf
Kellen James <Kellenkjames@gmail.com>
2023-01-23 18:24:19 -0500

    Add examples and test cases

--- easy_3/searching_101.rb
+++ easy_3/searching_101.rb
@@ -23,6 +23,20 @@ __________________________________________________
 
 * E - Examples / Test Cases
 
+==> Enter the 1st number:
+25
+==> Enter the 2nd number:
+15
+==> Enter the 3rd number:
+20
+==> Enter the 4th number:
+17
+==> Enter the 5th number:
+23
+==> Enter the last number:
+17
+The number 17 appears in [25, 15, 20, 17, 23].
+
 __________________________________________________
 
 * D - Data Structure
