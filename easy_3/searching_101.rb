64755911e7b4965ef4497ef26c53cb172cb8c636
Kellen James <Kellenkjames@gmail.com>
2023-01-23 18:22:11 -0500

    Add folder for easy_3 and searching_101 file

---
+++ easy_3/searching_101.rb
@@ -0,0 +1,41 @@
+# Searching 101
+
+=begin
+
+* P - [Understanding the] Problem
+
+Problem: Write a program that solicits 6 numbers from the user, then prints a message that describes whether or not the 6th number appears amongst the first 5 numbers.
+
+input: integers (6)
+output: string
+rules:
+        - Explicit Requirements:
+          - Ask the user to enter 6 numbers
+          - Print a message that describes whether or not the 6th number appears amongst the first 5 numbers
+
+        - Implicit Requirements:
+          - User input (string) needs to be converted to integer object
+
+        - Clarifying Questions:
+          - N / A
+
+__________________________________________________
+
+* E - Examples / Test Cases
+
+__________________________________________________
+
+* D - Data Structure
+
+__________________________________________________
+
+* A - Algorithm
+
+__________________________________________________
+
+=end
+
+#_________________________________________________
+
+#* C - Code
+
