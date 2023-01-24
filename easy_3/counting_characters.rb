37f241edbbfd05968c67b339042c4f84dfd422b4
Kellen James <Kellenkjames@gmail.com>
2023-01-24 11:30:49 -0500

    Add new file and problem description

---
+++ easy_3/counting_characters.rb
@@ -0,0 +1,42 @@
+# Counting the Number of Characters
+
+=begin
+
+* P - [Understanding the] Problem
+
+Problem: Write a program that will ask a user for an input of a word or multiple words and give back the number of characters. Spaces should not be counted as a character.
+
+input: string
+output: string
+rules:
+        - Explicit Requirements:
+          - Ask a user for an input of a word or multiple words
+          - Give back the number of characters
+
+        - Implicit Requirements:
+          - Spaces should not be counted as a character
+
+        - Clarifying Questions:
+          - Can the string contain non-alphabetical characters
+
+__________________________________________________
+
+* E - Examples / Test Cases
+
+
+__________________________________________________
+
+* D - Data Structure
+
+__________________________________________________
+
+* A - Algorithm
+
+__________________________________________________
+
+=end
+
+#_________________________________________________
+
+#* C - Code
+
