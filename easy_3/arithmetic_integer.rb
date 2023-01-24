8ae38bf39b0d488d4cde83fcb1fcdc9e5ef88827
Kellen James <Kellenkjames@gmail.com>
2023-01-24 10:30:00 -0500

    Add new file for arithmetic_integer and problem description

---
+++ easy_3/arithmetic_integer.rb
@@ -0,0 +1,45 @@
+# Arithmetic Integer
+
+=begin
+
+* P - [Understanding the] Problem
+
+Problem: Write a program that prompts the user for two positive integers, and then prints the results of the following operations on those two numbers: addition, subtraction, product, quotient, remainder, and power. Do not worry about validating the input.
+
+input(s): two positive integers
+output: integer
+rules:
+        - Explicit Requirements:
+          - Prompt the user for two positive integers
+          - Print the results of the following operations on those two numbers:
+            - addition
+            - subtraction
+            - product
+            - quotient
+
+        - Implicit Requirements:
+          - Do not worry about validating the input
+
+        - Clarifying Questions:
+          - N / A
+
+__________________________________________________
+
+* E - Examples / Test Cases
+
+__________________________________________________
+
+* D - Data Structure
+
+__________________________________________________
+
+* A - Algorithm
+
+__________________________________________________
+
+=end
+
+#_________________________________________________
+
+#* C - Code
+
