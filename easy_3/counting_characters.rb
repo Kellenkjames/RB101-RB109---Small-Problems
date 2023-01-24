c6fccf1f455a1cba1de0f5fe1e23603151b0df60
Kellen James <Kellenkjames@gmail.com>
2023-01-24 12:53:10 -0500

    Add code solution

--- easy_3/counting_characters.rb
+++ easy_3/counting_characters.rb
@@ -17,7 +17,7 @@ rules:
           - Spaces should not be counted as a character
 
         - Clarifying Questions:
-          - Can the string contain non-alphabetical characters
+          - Can the string contain non-alphabetical characters? 
 
 __________________________________________________
 
@@ -33,9 +33,26 @@ __________________________________________________
 
 * D - Data Structure
 
+Array []
+
 __________________________________________________
 
 * A - Algorithm
+
+- Initialize a variable called chars and assign an empty array []
+
+- Prompt the user to write a word or multiple words:
+  - words = gets.chomp.downcase
+
+- Iterate over each character words
+  - For each iteration:
+    - Delete spaces " "
+    - chars << char
+  - end
+
+  - chars.count
+
+  print "There are chars.count in in #{words}"
 
 __________________________________________________
 
@@ -45,3 +62,15 @@ __________________________________________________
 
 #* C - Code
 
+chars = []
+
+print 'Please write word or multiple words: '
+words = gets.chomp.downcase
+
+modified_words = words.delete " " 
+
+modified_words.each_char do |char|
+  chars << char
+end
+
+puts "There are #{chars.count} characters in #{words}."
