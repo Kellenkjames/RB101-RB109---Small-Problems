b19cc1806de7fb62a24c3da81d6001dd41530fe2
Kellen James <Kellenkjames@gmail.com>
2023-01-26 15:08:31 -0500

    Add new file for exclusive_or

---
+++ easy_3/exclusive_or.rb
@@ -0,0 +1,42 @@
+# Exclusive Or
+
+=begin
+
+* P - [Understanding the] Problem
+
+Problem: Write a method named `xor` that takes two arguments, and returns `true` if exactly one of its arguments is truthy, `false` otherwise.
+
+input: two booleans
+output: boolean
+rules:
+        - Explicit Requirements:
+          - Method will take two arguments
+          - Returns `true` if exactly one of its arguments is truthy
+          - False otherwise
+
+        - Implicit Requirements:
+          - Return a boolean result (not a truthy/falsey value)
+
+        - Clarifying Questions:
+          - N / A
+
+__________________________________________________
+
+* E - Examples / Test Cases
+
+__________________________________________________
+
+* D - Data Structure
+
+__________________________________________________
+
+* A - Algorithm
+
+__________________________________________________
+
+=end
+
+#_________________________________________________
+
+#* C - Code
+
