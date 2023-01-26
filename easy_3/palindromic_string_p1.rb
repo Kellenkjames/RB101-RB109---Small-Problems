f38373185be68bfac66367817493f13fc470b1a3
Kellen James <Kellenkjames@gmail.com>
2023-01-26 16:16:01 -0500

    Add high-level details for algorithm

--- easy_3/palindromic_string_p1.rb
+++ easy_3/palindromic_string_p1.rb
@@ -4,13 +4,13 @@
 
 * P - [Understanding the] Problem
 
-Problem: Write a method that returns true if the string passed as an argument is a palindrome, false otherwise. A palindrome reads the same forward and backward. 
+Problem: Write a method that returns true if the string passed as an argument is a palindrome, false otherwise. A palindrome reads the same forward and backward.
 
-input: 
-output: 
+input: string
+output: boolean
 rules:
         - Explicit Requirements:
-          - Return true if the string is passed as an argument, false otherwise. 
+          - Return true if the string is passed as an argument, false otherwise.
 
         - Implicit Requirements:
           - Case matters as does punctuation and spaces.
@@ -22,13 +22,27 @@ __________________________________________________
 
 * E - Examples / Test Cases
 
+palindrome?('madam') == true
+palindrome?('Madam') == false          # (case matters)
+palindrome?("madam i'm adam") == false # (all characters matter)
+palindrome?('356653') == true
+
 __________________________________________________
 
 * D - Data Structure
 
+N / A
+
 __________________________________________________
 
 * A - Algorithm
+
+- Initialize variable called reversed_string and assign to reversed string argument
+- If reversed_string == string
+  return true
+- Else
+  return false
+end
 
 __________________________________________________
 
