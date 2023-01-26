ccde08893574af682d1e79c3d43f3cba27f5f570
Kellen James <Kellenkjames@gmail.com>
2023-01-26 15:36:30 -0500

    Add new file and problem description for palindromic_string

---
+++ easy_3/palindromic_string_p1.rb
@@ -0,0 +1,40 @@
+# Palindromic Strings (Part 1)
+
+=begin
+
+* P - [Understanding the] Problem
+
+Problem: Write a method that returns true if the string passed as an argument is a palindrome, false otherwise. A palindrome reads the same forward and backward. 
+
+input: 
+output: 
+rules:
+        - Explicit Requirements:
+          - Return true if the string is passed as an argument, false otherwise. 
+
+        - Implicit Requirements:
+          - Case matters as does punctuation and spaces.
+
+        - Clarifying Questions:
+          - N / A
+
+__________________________________________________
+
+* E - Examples / Test Cases
+
+__________________________________________________
+
+* D - Data Structure
+
+__________________________________________________
+
+* A - Algorithm
+
+__________________________________________________
+
+=end
+
+#_________________________________________________
+
+#* C - Code
+
