2301c06e45f3ab6fadf3f93dd6bb00268a18aa5d
Kellen James <Kellenkjames@gmail.com>
2023-01-26 16:59:20 -0500

    Add additional methods to address more use cases

--- easy_3/palindromic_string_p1.rb
+++ easy_3/palindromic_string_p1.rb
@@ -55,3 +55,38 @@ palindrome?('madam') == true
 palindrome?('Madam') == false          # (case matters)
 palindrome?("madam i'm adam") == false # (all characters matter)
 palindrome?('356653') == true
+
+#_________________________________________________
+
+#* Further Exploration
+
+# Write a method that determines whether an array is palindromic; that is, the element values appear in the same sequence both forwards and backwards in the array. 
+
+def arr_palindrome?(arr)
+  arr == arr.reverse
+end
+
+arr_palindrome?(['foo', 'bar', 'foo']) == true
+arr_palindrome?([1, 2, 1]) == true
+arr_palindrome?(['2', '5', '5', '7']) == false
+
+#_________________________________________________
+
+# Now write a method that determines whether an array or a string is palindromic; that is, write a method that can take either an array or a string argument, and determines whether that argument is a palindrome. You may not use an if, unless, or case statement or modifier.
+
+def object_palindrome?(object)
+  arr = []
+  
+  counter = 0
+  loop do
+    arr << object[counter]
+    counter += 1
+    break if counter == object.size
+  end
+  
+  arr == arr.reverse
+end
+
+object_palindrome?([1, 2, 1])
+object_palindrome?('madam')
+object_palindrome?(['foo', 'bar', 'foo'])
