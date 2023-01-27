f81b89d2cd830282bf9be3dc141f284bf78a7de6
Kellen James <Kellenkjames@gmail.com>
2023-01-27 10:35:47 -0500

    Add high-level details for problem description

--- easy_3/palindromic_string_p3.rb
+++ easy_3/palindromic_string_p3.rb
@@ -4,19 +4,19 @@
 
 * P - [Understanding the] Problem
 
-Problem: 
+Problem: Write a method that returns true if its integer argument is palindromic, false otherwise. A palindromic number reads the same forwards and backwards.
 
-input: 
-output: 
+input: integer
+output: boolean
 rules:
         - Explicit Requirements:
-          -
+          - Method returns true if its integer argument is palindromic, false otherwise. 
 
         - Implicit Requirements:
-          - 
+          - Method will only accept one integer argument
 
         - Clarifying Questions:
-          -
+          - N / A
 
 __________________________________________________
 
