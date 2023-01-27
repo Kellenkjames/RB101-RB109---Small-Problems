c61064e964a93275464dd686ae8f94aa90e5d53c
Kellen James <Kellenkjames@gmail.com>
2023-01-27 11:13:50 -0500

    Add problem description and provided test cases

---
+++ easy_3/uppercase_check.rb
@@ -0,0 +1,50 @@
+# Uppercase Check
+
+=begin
+
+* P - [Understanding the] Problem
+
+Problem: Write a method that takes a string argument, and returns true if all of the alphabetic characters inside the string are uppercase, false otherwise. Characters that are not alphabetic should be ignored.
+
+input: string
+output: boolean
+rules:
+        - Explicit Requirements:
+          - Method takes a string argument:
+          - Returns true if all of the alphabetic characters inside the string are uppercse
+          - False otherwise
+
+        - Implicit Requirements:
+          - Non-alphabetical characters should be ignored
+          - Return true if string is empty ('')
+
+        - Clarifying Questions:
+          - N/A
+
+__________________________________________________
+
+* E - Examples / Test Cases
+
+uppercase?('t') == false
+uppercase?('T') == true
+uppercase?('Four Score') == false
+uppercase?('FOUR SCORE') == true
+uppercase?('4SCORE!') == true
+uppercase?('') == true
+
+__________________________________________________
+
+* D - Data Structure
+
+__________________________________________________
+
+* A - Algorithm
+
+__________________________________________________
+
+=end
+
+#_________________________________________________
+
+#* C - Code
+
