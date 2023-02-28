e90a8724d1ddbb8d9ee9473ff8b8cf042d46186f
Kellen James <Kellenkjames@gmail.com>
2023-02-28 14:11:24 -0500

    Checkpoint, add logic for first part of mini-algorithm

--- easy_4/what_century_is_that.rb
+++ easy_4/what_century_is_that.rb
@@ -18,11 +18,11 @@ rules:
           - New centuries begin in years that end with `01`
 
         - Implicit Requirements:
-          - The input will need to be converted to a mutable object, i.e., String
-          - The number of digits in the input will be a factor in determining the century
-
+          - The input doesn't necessarily determine the century year (with no other actions).
+      
         - Clarifying Questions:
           - Are we saying that any year ending with `01` is a new century?
+          - What is a century?
 
 __________________________________________________
 
@@ -42,9 +42,54 @@ __________________________________________________
 
 * D - Data Structure
 
+N / A
+
 __________________________________________________
 
 * A - Algorithm
+
+# Mini-Problem: Determine the century number, i.e., 1
+
+- Initialize a local variable called century and assign to`nil`
+- Initialize a local variable called year_to_str and assign to return value of year.to_s
+
+- if year.between?(1, 100)
+  century = 1
+- elsif year.between?(101, 200)
+  century = 2
+- elsif year.between?(201, 300)
+  century = 3
+- elsif year.between?(301, 400)
+  century = 4
+- elsif year.between?(401, 500)
+  century = 5
+- elsif year.between?(501, 600)
+  century = 6
+- elsif year.between?(601, 700)
+  century = 7
+- elsif year.between?(701, 800)
+  century = 8
+- elsif year.between?(801, 900)
+  century = 9
+- elsif year.between?(901, 1000)
+  century = 10
+end
+
+#* What sequnce do we see here? 
+- With the exception of the first century, every new century ends with `01`
+- From centuries 1-10 (excluding the end of the 10th century, `1000`) the input is between 1-3 digits
+
+- We need to do some manual work until we reach the 11th century
+
+century(1) == 1
+century(33) == 1
+century(100) == 1
+
+century(101) == 2
+century(136) == 2
+century(200) == 2
+
+
 
 __________________________________________________
 
