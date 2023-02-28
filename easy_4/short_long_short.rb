40738942b7e38416160d51bad1191c08aa1f92f2
Kellen James <Kellenkjames@gmail.com>
2023-02-28 11:13:11 -0500

    Add solution to short_long_short using PEDAC

---
+++ easy_4/short_long_short.rb
@@ -0,0 +1,66 @@
+=begin
+
+* P - [Understanding the] Problem
+
+Problem: Write a method that takes two strings as arguments, determines the longest of the two strings, and then returns the result of concatenating the shorter string, the longer string, and the shorter string once again. You may assume that the strings are of different lengths.
+
+input: two strings
+output: a new string
+rules:
+        - Explicit Requirements:
+          - Method will take two strings as arguments
+          - Determines the longest of the two strings
+          - Returns the result of concatenating the shorter string, the longer string, and the shorter string once again
+
+        - Implicit Requirements:
+          - Both string arguments will have different lengths
+
+        - Clarifying Questions:
+          - N / A
+
+__________________________________________________
+
+* E - Examples / Test Cases
+
+short_long_short('abc', 'defgh') == "abcdefghabc"
+short_long_short('abcde', 'fgh') == "fghabcdefgh"
+short_long_short('', 'xyz') == "xyz"
+
+__________________________________________________
+
+* D - Data Structure
+
+N / A
+
+__________________________________________________
+
+* A - Algorithm
+
+Determine which string is the longest of two strings:
+
+def short_long_short(str_one, str_two)
+  - if str_one.size > str_two.size
+    str_two + str_one + str_two
+  - else
+    str_one + str_two + str_one
+  end
+end
+__________________________________________________
+
+=end
+
+#_________________________________________________
+
+#* C - Code
+
+def short_long_short(str_one, str_two)
+  if str_one.size > str_two.size
+    str_two + str_one + str_two
+  else
+    str_one + str_two + str_one
+  end
+end
+
+short_long_short('abc', 'defgh') == "abcdefghabc"
+short_long_short('abcde', 'fgh') == "fghabcdefgh"
+short_long_short('', 'xyz') == "xyz"
