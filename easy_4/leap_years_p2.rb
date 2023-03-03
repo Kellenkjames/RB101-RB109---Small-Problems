0a8b2dcc94c8cdc5d4d2a06776817d90e74b52aa
Kellen James <Kellenkjames@gmail.com>
2023-03-03 10:36:53 -0500

    Add PEDAC solution for leap_years with new rules

--- easy_4/leap_years_p2.rb
+++ easy_4/leap_years_p2.rb
@@ -2,20 +2,19 @@
 
 * P - [Understanding the] Problem
 
-Problem: In the modern era under the Gregorian Calendar, leap years occur in every year that is evenly divisible by 4, unless the year is also divisible by 100. If the year is evenly divisible by 100, then it is not a leap year unless the year is evenly divisible by 400.
+Problem: A continuation of the previous exercise. The British Empire adopted the Gregorian Calendar in 1752, which was a leayear. Prior to 1752, the Julian Calendar was used. Under the Julian Calendar, leayears occur in any year that is evenly divisible by 4.
 
-Assume this rule is good for any year greater than year 0. Write a method that takes any year greater than 0 as input, and returns true if the year is a leap year, or false if it is not a leap year.
+Using this information, update the method from the previous exercise to determine leayears both before and after 1752.
 
 input: Integer
 output: Boolean
 rules:
         - Explicit Requirements:
-          - Leap years occur in every year that is evenly divisible by 4 and not divisible by 100
-            - If the year is evenly divisible by 100 it must also be divsible by 400
-          - Returns true if the year is a leap year, or false if is not a leap year
-          
+          - Before the year 1752, leayear occurs in any year that is evenly divisible by 4
+          - After the year 1752, leayear follows the rules of the Gregorian Calendar in 1752
+
         - Implicit Requirements:
-          - Year will always be greater than 0
+          - Input will always be greater than 1
 
         - Clarifying Questions:
           - N / A
@@ -33,9 +32,9 @@ leap_year?(240001) == false
 leap_year?(2000) == true
 leap_year?(1900) == false
 leap_year?(1752) == true
-leap_year?(1700) == false
+leap_year?(1700) == true
 leap_year?(1) == false
-leap_year?(100) == false
+leap_year?(100) == true
 leap_year?(400) == true
 
 __________________________________________________
@@ -48,15 +47,14 @@ __________________________________________________
 
 * A - Algorithm
 
-Define a method that accepts a parameter:
-
-if year % 4 == 0 && year % 100 != 0
-    return true
-  elsif year % 100 == 0 && year % 400 == 0
-    return true
-  else
-    return false
-  end
+- if year <= 1752 && year % 4 == 0
+  return true
+-elsif (year % 4 == 0) && (year % 100 != 0)
+  return true
+- elsif (year % 100 == 0) && (year % 400 == 0)
+  return true
+- else
+  return false
 end
 
 __________________________________________________
@@ -68,7 +66,9 @@ __________________________________________________
 #* C - Code
 
 def leap_year?(year)
-  if (year % 4 == 0) && (year % 100 != 0)
+  if year <= 1752 && year % 4 == 0
+    return true
+  elsif (year % 4 == 0) && (year % 100 != 0)
     return true
   elsif (year % 100 == 0) && (year % 400 == 0)
     return true
@@ -76,7 +76,6 @@ def leap_year?(year)
     return false
   end
 end
-
 
 leap_year?(2016) == true
 leap_year?(2015) == false
@@ -87,7 +86,7 @@ leap_year?(240001) == false
 leap_year?(2000) == true
 leap_year?(1900) == false
 leap_year?(1752) == true
-leap_year?(1700) == false
+leap_year?(1700) == true
 leap_year?(1) == false
-leap_year?(100) == false
+leap_year?(100) == true
 leap_year?(400) == true
