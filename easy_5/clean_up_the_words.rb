957df5c4a6f76dd3d00460c7480ee34347238dc7
Kellen James <Kellenkjames@gmail.com>
2023-03-12 18:44:11 -0400

    Update inputs and outputs for multiple files

--- easy_5/clean_up_the_words.rb
+++ easy_5/clean_up_the_words.rb
@@ -2,12 +2,10 @@
 
 * P - [Understanding the] Problem
 
-Problem: Given a string that consists of some words (all lowercased) and an assortment of non-alphabetic characters, write a method that returns that string with all of the non-alphabetic characters replaced by spaces.
-
-If one or more non-alphabetic characters occur in a row, you should only have one space in the result (the result should never have consecutive spaces).
+Given a string that consists of some words (all lowercased) and an assortment of non-alphabetical characters, write a method that returns that string with all of the non-alphabetic characters replaced by spaces. If one or more non-alphabetic characters occurs in a row, you should only have one space in the result (the result should never have consecutive spaces).
 
 input: String
-output: String (new object)
+output: String (original object)
 rules:
         - Explicit Requirements:
           - Method will return a String with all of the non-alphabetic characters replaced by spaces
@@ -15,6 +13,8 @@ rules:
           
         - Implicit Requirements:
           - The result should never have consecutive spaces
+          - String is case insensitive
+          - Non-Alphanumeric characters are the other characters on your keyboard that aren't letters or numbers, e.g. commas, brackets, space, asterisk and so on.
 
         - Clarifying Questions:
           - N / A
@@ -29,9 +29,23 @@ __________________________________________________
 
 * D - Data Structure
 
+# => Array []
+
 __________________________________________________
 
 * A - Algorithm
+
+char.match(/^[[:alpha:]]$/)
+
+- Split the String input into an Array of substrings
+- Split each Array of substrings into chars
+  - Iterate over the collection of chars:
+    - For each iteration, assign the current element to an empty space if the element is a non-alphanumeric char
+  - Repeat this step until all non-alphanumeric chars have been repalced with empty spaces
+  
+  - Join Array of substrings to String
+  - Substitute two or more empty spaces with single empty space
+  - Return String
 
 __________________________________________________
 
@@ -41,3 +55,14 @@ __________________________________________________
 
 #* C - Code
 
+def cleanup(string)
+  string = string.split(' ').map do |substring|
+    substring.chars.map do |char|
+      !char.match(/[[:alpha:]]/) ? char = ' ' : char
+    end
+  end
+  # string.join.gsub(/[[:space:]]{2,}/, ' ')
+end
+
+p cleanup("---what's my +*& line?")    # == ' what s my line '
+# p cleanup("---what's my +*& line?") # == ' what smy line '
